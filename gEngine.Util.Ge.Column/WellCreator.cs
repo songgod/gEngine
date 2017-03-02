6e16d67a02c5380ec7904c5789912ee8d06acf32
sjm <sjm@gptoil.com>
2017-03-02 15:51:19 +0800

    集成多井柱状图到新建连井剖面功能上，同时处理调整深度道使左右各一条曲线

--- gEngine.Util.Ge.Column/WellCreator.cs
+++ gEngine.Util.Ge.Column/WellCreator.cs
@@ -9,29 +9,6 @@ namespace gEngine.Util.Ge.Column
 {
     public class WellCreator
     {
-        //public IObjects Create(IDBWell db)
-        //{
-
-        //    if (db == null)
-        //        return null;
-
-        //    IObjects res = new IObjects();
-
-        //    Well well = new Well() { Name = db.Name };
-        //    well.Depths = new Utility.ObsDoubles(db.Depths);
-
-        //    for (int i = 0; i < db.Columns.Count; i++)
-        //    {
-        //        string name = db.Columns[i].Item1;
-        //        WellColumn c = new WellColumn() { Name = db.Columns[i].Item1, Owner = well, MathType = Enums.MathType.DEFAULT };
-        //        c.Values = new Utility.ObsDoubles(db.Columns[i].Item2);
-        //        c.Color = Colors.Red;
-        //        well.Columns.Add(c);
-        //    }
-        //    res.Add(well);
-        //    return res;
-        //}
-
         public IObjects Create(IDBWell db)
         {
             Random rdm = new Random();
@@ -56,8 +33,8 @@ namespace gEngine.Util.Ge.Column
                     c.Values = new Utility.ObsDoubles(db.Columns[i].Item2);
                     //c.Color =  Colors.Red;
                     c.Color = Color.FromRgb((byte)rdm.Next(0, 255), (byte)rdm.Next(0, 255), (byte)rdm.Next(0, 255));
-                    // 初步定义深度道显示在第二条曲线右侧
-                    if (i.Equals(2))
+                    // 初步定义深度道显示在第一条曲线右侧
+                    if (i.Equals(1))
                     {
                         colsOffset = depthsWidth;
                         well.DepthsOffset = i * 60 + wellColumnWidth + j * 100;
