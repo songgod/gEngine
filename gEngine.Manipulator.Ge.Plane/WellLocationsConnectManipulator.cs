606b77ede07c9b73a3fca335f97aadba86925565
songgod <songgod@163.com>
2017-03-10 13:41:02 +0800

    增加连井操作方式

---
+++ gEngine.Manipulator.Ge.Plane/WellLocationsConnectManipulator.cs
@@ -0,0 +1,55 @@
+﻿using gEngine.Graph.Ge;
+using gEngine.Graph.Ge.Plane;
+using gEngine.Manipulator;
+using gEngine.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace gEngine.Manipulator.Ge.Plane
+{
+    public class WellLocationsConnectManipulator : PolyLineManipulator
+    {
+        public List<string> SelectWellLocations { get; set; }
+        public delegate void FinishSelectWellLocations(List<string> names);
+        public event FinishSelectWellLocations OnFinishSelect;
+
+        public override void MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            LayerControl lc = this.AssociatedObject as LayerControl;
+
+            Point p = e.GetPosition(lc);
+            HitTestResult hr = VisualTreeHelper.HitTest(lc, p);
+            if (hr==null || hr.VisualHit == null)
+                return;
+
+            Shape sp = hr.VisualHit as Shape;
+            if (sp == null)
+                return;
+            WellLocation wl = sp.DataContext as WellLocation;
+
+            if (wl==null)
+                return;
+
+            SelectWellLocations.Add(wl.Name);
+
+            base.MouseLeftButtonUp(sender, e);
+        }
+
+        public override void MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (OnFinishSelect != null && SelectWellLocations.Count != 0)
+                OnFinishSelect.Invoke(SelectWellLocations);
+
+            base.MouseRightButtonUp(sender, e);
+        }
+    }
+}
---
+++ gEngine.Manipulator/PolyLineManipulator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+using System.Windows.Input;
+
+namespace gEngine.Manipulator
+{
+    public class PolyLineManipulator : ManipulatorBase
+    {
+        protected PolyTrackAdorner TrackAdorner { get; set; }
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (this.AssociatedObject == null)
+                return;
+
+            TrackAdorner = new PolyTrackAdorner(this.AssociatedObject);
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.AssociatedObject);
+            adornerLayer.Add(TrackAdorner);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this.AssociatedObject);
+            adornerLayer.Remove(this.TrackAdorner);
+        }
+
+        public override void MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            this.TrackAdorner.RemoveLastPoint();
+            this.TrackAdorner.AddPoint(e.GetPosition(this.AssociatedObject));
+            this.TrackAdorner.AddPoint(e.GetPosition(this.AssociatedObject));
+            base.MouseLeftButtonUp(sender, e);
+        }
+
+        public override void MouseMove(object sender, MouseEventArgs e)
+        {
+            this.TrackAdorner.MoveLastPoint(e.GetPosition(this.AssociatedObject));
+        }
+    }
+}
