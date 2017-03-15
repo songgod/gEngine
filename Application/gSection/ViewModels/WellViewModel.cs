a81751c85f68b8b5ea049560ab714c16c6ae2948
sjm <sjm@gptoil.com>
2017-03-15 15:41:26 +0800

    1、多井柱状图逻辑代码分离，采用viewModel模式处理 2、新增柱状图设置页面

---
+++ Application/gSection/ViewModels/WellViewModel.cs
@@ -0,0 +1,106 @@
+﻿using gEngine.Data.Ge.Txt;
+using gEngine.Graph.Ge;
+using gEngine.Graph.Ge.Column;
+using gEngine.Graph.Interface;
+using gEngine.Util;
+using gEngine.Util.Ge.Column;
+using gEngine.View;
+using GPTDxWPFRibbonApplication1.Controls;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace GPTDxWPFRibbonApplication1.ViewModels
+{
+    public class WellViewModel
+    {
+        private FrameworkElement _mc;
+        public FrameworkElement mc
+        {
+            get { return _mc; }
+            set
+            {
+                _mc = value;
+                //InitWell(_mc);
+            }
+        }
+
+        public WellViewModel()
+        {
+        }
+
+        #region Method
+
+        private void InitWell(FrameworkElement mc)
+        {
+            Random rdm = new Random();
+            Map map = new Map();
+            Layer layer = new Layer();
+
+            string wellNums = New_section_set.WellNums;
+            int wellCount = 0;
+            int WellWidthSum = 0;
+
+            if (!string.IsNullOrEmpty(wellNums))
+            {
+                string[] split = wellNums.Split(',');
+                foreach (string wellNum in split)
+                {
+                    string txtfilePath = "D:\\Data\\" + wellNum + ".txt";
+                    if (File.Exists(txtfilePath))
+                    {
+                        TxtWell tw = new TxtWell() { TxtFile = txtfilePath };
+                        WellCreator wc = new WellCreator();
+                        IObjects res = new IObjects();
+                        res = wc.Create(tw);
+                        foreach (Well well in res)
+                        {
+                            well.Name = wellNum.ToString();
+                            well.LongitudinalProportion = 1500;//纵向比例
+                            int depthsWidth = 60;// 深度道宽度
+                            int colsOffset = 0; // 曲线偏移
+                            well.Location = wellCount == 0 ? 0 : WellWidthSum;
+                            WellWidthSum += (well.Columns.Count * 60 + depthsWidth) + 50;
+                            for (int i = 0; i < well.Columns.Count; i++)
+                            {
+                                // 初步定义深度道显示在第一条曲线右侧
+                                if (i.Equals(1))
+                                {
+                                    colsOffset = depthsWidth;
+                                    well.DepthsOffset = i * 60;
+                                }
+                                well.Columns[i].Color = Color.FromRgb((byte)rdm.Next(0, 255), (byte)rdm.Next(0, 255), (byte)rdm.Next(0, 255));
+                                well.Columns[i].Offset = colsOffset + i * 60;
+                            }
+
+                            wellCount++;
+                            layer.Objects.Add(well);
+                        }
+                    }
+                }
+            }
+
+            map.Layers.Add(layer);
+            Binding bd = new Binding("Layers") { Source = map };
+            mc.SetBinding(ItemsControl.ItemsSourceProperty, bd);
+        }
+                
+        #region ICommand
+
+        public System.Windows.Input.ICommand LoadCommand
+        {
+            get { return new RelayCommand<FrameworkElement>(InitWell); }
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
