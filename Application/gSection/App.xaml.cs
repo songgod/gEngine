d58f39ec28e81d48aa2495b23d06323822865844
songgod <songgod@163.com>
2017-04-06 17:44:16 +0800

    最初gSection界面

--- Application/gSection/App.xaml.cs
+++ Application/gSection/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace GPTDxWPFRibbonApplication1
+namespace gSection
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Application/gSection/Converters/ObjectsEqualityConverter.cs
+++ Application/gSection/Converters/ObjectsEqualityConverter.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace GPTDxWPFRibbonApplication1.Converters
+namespace gSection.Converters
 {
     public class ObjectsEqualityConverter : IValueConverter
     {
