01b5829372cebccbc91a8264e57393a5b7a67888
songgod <songgod@163.com>
2017-04-06 17:50:15 +0800

    疏漏文件

---
+++ Application/gSection/View/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DevExpress.Xpf.Ribbon;
+
+namespace gSection.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DXRibbonWindow
+    {
+
+        public string[] ClipartImages { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ClipartImages = new string[] {
+                 "/RibbonDemo;component/Images/Clipart/caCompClient.png",
+                 "/RibbonDemo;component/Images/Clipart/caCompClientEnabled.png",
+                 "/RibbonDemo;component/Images/Clipart/caDatabaseBlue.png",
+                 "/RibbonDemo;component/Images/Clipart/caDataBaseDisabled.png",
+                 "/RibbonDemo;component/Images/Clipart/caDataBaseGreen.png",
+                 "/RibbonDemo;component/Images/Clipart/caDataBaseViolet.png",
+                 "/RibbonDemo;component/Images/Clipart/caInet.png",
+                 "/RibbonDemo;component/Images/Clipart/caInetSearch.png",
+                 "/RibbonDemo;component/Images/Clipart/caModem.png",
+                 "/RibbonDemo;component/Images/Clipart/caModemEnabled.png",
+                 "/RibbonDemo;component/Images/Clipart/caNetCard.png",
+                 "/RibbonDemo;component/Images/Clipart/caNetwork.png",
+                 "/RibbonDemo;component/Images/Clipart/caNetworkEnabled.png",
+                 "/RibbonDemo;component/Images/Clipart/caServer.png",
+                 "/RibbonDemo;component/Images/Clipart/caServerEnabled.png",
+                 "/RibbonDemo;component/Images/Clipart/caWebCam.png"
+             };
+        }
+    }
+}
