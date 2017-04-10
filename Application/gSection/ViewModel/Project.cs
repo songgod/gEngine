a39f66169fc82c8f15bcea94d12afc766fb0da01
songgod <songgod@163.com>
2017-04-10 18:34:19 +0800

    重构

---
+++ Application/gSection/ViewModel/Project.cs
@@ -0,0 +1,24 @@
+﻿using gEngine.Data.Interface;
+using gEngine.Graph.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gSection.ViewModel
+{
+    public class Project
+    {
+        public Project() 
+        {
+
+        }
+
+        public IMaps Maps { get; set; }
+
+        public IMaps OpenMaps { get; set; }
+
+        public IDBFactory DBFactory { get; set; }
+    }
+}
