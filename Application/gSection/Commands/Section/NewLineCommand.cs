4b68b1822c1a38d1d2f7935c190117501d8a8297
songgod <songgod@163.com>
2017-04-11 11:50:31 +0800

    重构

---
+++ Application/gSection/Commands/Section/NewLineCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace gSection.Commands.Section
+{
+    public class NewLineCommand : Command
+    {
+        public override bool CanExecute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/gSection/View/TabControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Globalization;
+using DevExpress.Xpf.Core;
+using gEngine.View;
+
+namespace gSection.View
+{
+/// <summary>
+/// TabControl.xaml 的交互逻辑
+/// </summary>
+public partial class TabControl : DXTabControl
+    {
+        public TabControl()
+        {
+            InitializeComponent();
+        }
+
+        public int MapControlCount
+        {
+            get
+            {
+                return Items.Count;
+            }
+        }
+
+        public MapControl GetMapControl(int i)
+        {
+            if (i < 0 || i >= MapControlCount)
+                return null;
+
+            var chd = VisualTreeHelper.GetChild(FastRenderPanel, SelectedIndex);
+            return VisualTreeHelper.GetChild(chd, 0) as MapControl;
+        }
+
+        public int ActiveMapControlIndex
+        {
+            get
+            {
+                return SelectedIndex;
+            }
+            set
+            {
+                SelectedIndex = value;
+            }
+        }
+
+        public MapControl ActiveMapControl
+        {
+            get
+            {
+                return GetMapControl(ActiveMapControlIndex);
+            }
+        }
+    }
+}
