b52f2236b54d242178e1c0dabaf356c8bd495505
songgod <songgod@163.com>
2017-04-26 16:17:17 +0800

    在IObject中增加DataTemplate属性，并在DataTemplateChooser中根据该属性在资源中查询

--- gEngine.Graph.Ge/Base.cs
+++ gEngine.Graph.Ge/Base.cs
@@ -14,5 +14,19 @@ namespace gEngine.Graph.Ge
         // Using a DependencyProperty as the backing store for Name.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty NameProperty =
             DependencyProperty.Register("Name", typeof(string), typeof(Base));
+
+
+
+        public string DataTemplate
+        {
+            get { return (string)GetValue(DataTemplateProperty); }
+            set { SetValue(DataTemplateProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DataTemplate.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataTemplateProperty =
+            DependencyProperty.Register("DataTemplate", typeof(string), typeof(Base));
+
+
     }
 }
--- gEngine.View/DataTemplateChooser.cs
+++ gEngine.View/DataTemplateChooser.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gEngine.Graph.Interface;
+using System;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -8,21 +9,13 @@ namespace gEngine.View
     {
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            if (item == null) throw new ArgumentException();
-            if (container == null) throw new ArgumentException();
-            DataTemplate itemDataTemplate = null;
-            itemDataTemplate = base.SelectTemplate(item, container);
-            if (itemDataTemplate != null) return itemDataTemplate;
+            ContentPresenter c = container as ContentPresenter;
+            IObject o = item as IObject;
+            if (c == null || o == null || String.IsNullOrEmpty(o.DataTemplate))
+                return null;
 
-            FrameworkElement itemContainer = container as FrameworkElement;
-            if (itemContainer == null) return null;
-
-            foreach (Type itemInterface in item.GetType().GetInterfaces())
-            {
-                itemDataTemplate = itemContainer.TryFindResource(new DataTemplateKey(itemInterface)) as DataTemplate;
-                if (itemDataTemplate != null) break;
-            }
-            return itemDataTemplate;
+            DataTemplate dt = c.TryFindResource(o.DataTemplate) as DataTemplate;
+            return dt;
         }
     }
 }
