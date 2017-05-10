3f615495f029898120bf361e0c358ee688f3ee57
zhc <zhc@gptoil.com>
2017-05-10 09:14:15 +0800

    指北针符号实现

---
+++ gEngine.Graph.Ge.Basic/Compress.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace gEngine.Graph.Ge.Basic
+{
+    /// <summary>
+    /// 指北针
+    /// </summary>
+    public class Compress : Object
+    {
+        public string CompressData
+        {
+            get { return (string)GetValue(CompressDataProperty); }
+            set { SetValue(CompressDataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Top.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CompressDataProperty =
+            DependencyProperty.Register("CompressData", typeof(string), typeof(Compress));
+
+
+        public double RotateAngle
+        {
+            get { return (double)GetValue(RotateAngleProperty); }
+            set { SetValue(RotateAngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Top.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RotateAngleProperty =
+            DependencyProperty.Register("RotateAngle", typeof(double), typeof(Compress));
+    }
+}
---
+++ gEngineTest.Compass/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using gEngine.Graph.Ge.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gEngineTest.Compass
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Compress cp = new Compress() { RotateAngle = 0, CompressData = "M79.66667,11.333333 L42.33301,144.33367 L79.99967,85.000516 L116.99933,144.66733 z" };
+            cont.Content = cp;
+        }
+    }
+}
