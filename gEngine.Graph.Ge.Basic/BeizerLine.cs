fce1c0d46377bd561982bfbddfffadbbdb5be047
songgod <songgod@163.com>
2017-06-02 11:35:39 +0800

    1.增加线对象 2.更改gSection的Release的首选win32编译选项

---
+++ gEngine.Graph.Ge.Basic/BeizerLine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace gEngine.Graph.Ge.Basic
+{
+    public class BeizerLine : Base
+    {
+
+
+        public PointCollection Points
+        {
+            get { return (PointCollection)GetValue(PointsProperty); }
+            set { SetValue(PointsProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Points.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PointsProperty =
+            DependencyProperty.Register("Points", typeof(PointCollection), typeof(BeizerLine), new PropertyMetadata(new PointCollection()));
+
+
+
+        public LineStyle LinStyle
+        {
+            get { return (LineStyle)GetValue(LinStyleProperty); }
+            set { SetValue(LinStyleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for LinStyle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LinStyleProperty =
+            DependencyProperty.Register("LinStyle", typeof(LineStyle), typeof(BeizerLine), new PropertyMetadata(new NormalLineStyle()));
+
+
+    }
+}
