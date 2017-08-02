84cdf7ff96b3cdcf0aa277e0e8ea21a96fcc6576
sjm <sjm@gptoil.com>
2017-08-02 13:29:49 +0800

    1、线编辑器结构创建

---
+++ gEngine.Manipulator.Ge.Basic/EditLineObjectManipulator.cs
@@ -0,0 +1,150 @@
+﻿using gEngine.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace gEngine.Manipulator.Ge.Basic
+{
+    public class EditLineObjectManipulator : ObjectManipulator
+    {
+        public Rectangle TrackAdorner1 { get; set; }
+        public Rectangle TrackAdorner2 { get; set; }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (this.AssociatedObject == null)
+                return;
+            ObjectControl oc = this.AssociatedObject;
+            LayerControl lc = oc.Owner;
+            MapControl mc = lc.Owner;
+            if (mc == null)
+                return;
+            Point p1 = ((gEngine.Graph.Ge.Basic.Line) oc.DataContext).Start;
+            Point p2 = ((gEngine.Graph.Ge.Basic.Line) oc.DataContext).End;
+
+            Style style = new Style();
+            style.Setters.Add(new Setter() { Property = Rectangle.StrokeProperty, Value = new SolidColorBrush(Color.FromRgb(120, 162, 239)) });
+            style.Setters.Add(new Setter() { Property = Rectangle.StrokeThicknessProperty, Value = 1.0 });
+            style.Setters.Add(new Setter() { Property = Rectangle.FillProperty, Value = Brushes.White });
+            style.Setters.Add(new Setter() { Property = Rectangle.CursorProperty, Value = Cursors.Hand });
+            style.Setters.Add(new Setter() { Property = Rectangle.WidthProperty, Value = 5.0 });
+            style.Setters.Add(new Setter() { Property = Rectangle.HeightProperty, Value = 5.0 });
+
+            TrackAdorner1 = new Rectangle() { Style = style };
+            TrackAdorner2 = new Rectangle() { Style = style };
+
+            Canvas.SetLeft(TrackAdorner1, p1.X - TrackAdorner1.Width / 2);
+            Canvas.SetTop(TrackAdorner1, p1.Y - TrackAdorner1.Height / 2);
+            Canvas.SetLeft(TrackAdorner2, p2.X - TrackAdorner2.Width / 2);
+            Canvas.SetTop(TrackAdorner2, p2.Y - TrackAdorner2.Height / 2);
+
+            mc.EditLayer.Children.Add(TrackAdorner1);
+            mc.EditLayer.Children.Add(TrackAdorner2);
+
+            // oc.MouseMove += Oc_MouseMove;
+
+            mc.MouseLeftButtonDown += Oc_MouseLeftButtonDown;
+
+        }
+
+        private  void Oc_MouseMove(object sender, MouseEventArgs e)
+        {
+            ObjectControl oc = this.AssociatedObject;
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed)
+            {
+               Point p =  e.GetPosition(oc);
+                //Point p = mc.Dp2LP(e.GetPosition(mc));
+                //this.TrackAdorner.X2 = p.X;
+                //this.TrackAdorner.Y2 = p.Y;
+            }
+        }
+
+
+        private void Oc_MouseLeftButtonDown(object sender, MouseEventArgs e)
+        {
+            if (this.AssociatedObject == null)
+                return;
+            LayerControl lc = this.AssociatedObject.Owner;
+            MapControl mc = lc.Owner;
+
+            Point pt = e.GetPosition(mc);
+            VisualTreeHelper.HitTest(mc, null,
+                            new HitTestResultCallback(MyHitTestResult), new PointHitTestParameters(pt));
+            
+
+            
+
+
+         
+
+
+            
+
+        }
+
+
+        private HitTestResultBehavior MyHitTestResult(HitTestResult hr)
+        {
+            LayerControl lc = this.AssociatedObject.Owner;
+            MapControl mc = lc.Owner;
+            DependencyObject p = hr.VisualHit;
+            while (p != null)
+            {
+                ObjectControl oc = p as ObjectControl;
+                if (oc != null)
+                {
+                    ((gEngine.Graph.Ge.Basic.Line) oc.DataContext).End = new Point(300, 300);
+                    return HitTestResultBehavior.Stop;
+                }
+                p = VisualTreeHelper.GetParent(p);
+            }
+            return HitTestResultBehavior.Continue;
+        }
+
+
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            LayerControl lc = this.AssociatedObject.Owner;
+            MapControl mc = lc.Owner;
+            if (mc == null)
+                return;
+            mc.EditLayer.Children.Clear();
+
+        }
+    }
+
+    public class EditLineObjectFactory : IObjectManipulatorFactory
+    {
+        public string Name
+        {
+            get
+            {
+               return  "EditLineObjectManipulator";
+            }
+        }
+
+        public Type SupportIObjectType
+        {
+            get
+            {
+                return typeof(gEngine.Graph.Ge.Basic.Line);
+            }
+        }
+
+        public IManipulatorBase CreateManipulator(object param)
+        {
+            EditLineObjectManipulator dm = new EditLineObjectManipulator();
+            return dm;
+        }
+    }
+}
