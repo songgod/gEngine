fe621265a6ac6c4288d4dd3c3fa36fec2d954aa6
zhc <zhc@gptoil.com>
2017-08-08 08:21:44 +0800

    砂体面、地层面RibbonPageCategory实现

---
+++ gEngine.Application.Ge.Section/SandFaceProxyObjectRibbonPageCategory.xaml.cs
@@ -0,0 +1,48 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gEngine.Application.Ge.Section
+{
+    /// <summary>
+    /// SandFaceProxyObjectRibbonPageCategory.xaml 的交互逻辑
+    /// </summary>
+    public partial class SandFaceProxyObjectRibbonPageCategory : GeRibbonPageCategory
+    {
+        public SandFaceProxyObjectRibbonPageCategory()
+        {
+            InitializeComponent();
+        }
+        public override Type SupportType
+        {
+            get
+            {
+                return typeof(Graph.Ge.Section.SandFaceProxyObject);
+            }
+        }
+        public ICommand SelectBarCommand
+        {
+            get
+            {
+                return new DelegateCommand<string[]>((parameter) =>
+                {
+                    Graph.Ge.Section.SandFaceProxyObject sfpo = this.DataContext as Graph.Ge.Section.SandFaceProxyObject;
+                    sfpo.FillStyle.SymbolLib = parameter[0] as string;
+                    sfpo.FillStyle.Symbol = parameter[1] as string;
+                });
+            }
+        }
+    }
+}
