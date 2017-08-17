c6f550121f76e51ab891d137da092aa6c7bf0c16
sjm <sjm@gptoil.com>
2017-08-17 11:18:24 +0800

    临时获取

--- gEngine.Application.Ge.Section/SectionManageRibbonPage.xaml.cs
+++ gEngine.Application.Ge.Section/SectionManageRibbonPage.xaml.cs
@@ -28,6 +28,33 @@ namespace gEngine.Application.Ge.Section
         {
             InitializeComponent();
             this.Name = "SectionManageRibbonPage";
+            BindLongitudinalProportion();
+
+            this.DataContext = this;
+        }
+
+        #region Property
+
+        public List<int> LongitudinalProportion
+        {
+            get { return (List<int>) GetValue(LongitudinalProportionProperty); }
+            set { SetValue(LongitudinalProportionProperty, value); }
+        }
+
+        public static readonly DependencyProperty LongitudinalProportionProperty =
+            DependencyProperty.Register("LongitudinalProportion", typeof(List<int>), typeof(SectionManageRibbonPage));
+
+        #endregion
+
+        private void BindLongitudinalProportion()
+        {
+            LongitudinalProportion = new List<int>();
+            LongitudinalProportion.Add(100);
+            LongitudinalProportion.Add(200);
+            LongitudinalProportion.Add(500);
+            LongitudinalProportion.Add(1000);
+            LongitudinalProportion.Add(1500);
+            LongitudinalProportion.Add(2000);
         }
     }
 }
